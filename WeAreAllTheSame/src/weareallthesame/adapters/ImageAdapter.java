fd4167c30cece7595a1f019402c62683c5fc80af
KireP <kire_m.j@hotmail.com>
2015-07-10 17:17:00 +0200

    GridView with categories

--- WeAreAllTheSame/src/weareallthesame/adapters/ImageAdapter.java
+++ WeAreAllTheSame/src/weareallthesame/adapters/ImageAdapter.java
@@ -34,7 +34,7 @@ public class ImageAdapter extends BaseAdapter {
 		if (convertView == null) {
 			// if it's not recycled, initialize some attributes
 			imageView = new ImageView(mContext);
-			imageView.setLayoutParams(new GridView.LayoutParams(120, 120));
+			imageView.setLayoutParams(new GridView.LayoutParams(170, 170));
 			imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
 			imageView.setPadding(8, 8, 8, 8);
 		} else {
@@ -44,20 +44,16 @@ public class ImageAdapter extends BaseAdapter {
 		imageView.setImageResource(mThumbIds[position]);
 		return imageView;
 	}
-
-	// references to our images
-	private Integer[] mThumbIds = { R.drawable.sample_2, R.drawable.sample_3,
-			R.drawable.sample_4, R.drawable.sample_5, R.drawable.sample_6,
-			R.drawable.sample_7, R.drawable.sample_0, R.drawable.sample_1,
-			R.drawable.sample_2, R.drawable.sample_3, R.drawable.sample_4,
-			R.drawable.sample_5, R.drawable.sample_6, R.drawable.sample_7,
-			R.drawable.sample_0, R.drawable.sample_1, R.drawable.sample_2,
-			R.drawable.sample_3, R.drawable.sample_4, R.drawable.sample_5,
-			R.drawable.sample_6, R.drawable.sample_7, R.drawable.sample_2,
-			R.drawable.sample_3, R.drawable.sample_4, R.drawable.sample_5,
-			R.drawable.sample_6, R.drawable.sample_7, R.drawable.sample_0,
-			R.drawable.sample_1, R.drawable.sample_2, R.drawable.sample_3,
-			R.drawable.sample_4 };
+	
+	private Integer[] mThumbIds = { 
+			R.drawable.letters, R.drawable.numbers,
+			R.drawable.forms, R.drawable.animals,
+			R.drawable.fruit_vegetables, R.drawable.weather,
+			R.drawable.seasons, R.drawable.months_days, 
+			R.drawable.day_details, R.drawable.colors_objects, 
+			R.drawable.prepositions, R.drawable.hygiene, 
+			R.drawable.body_parts, R.drawable.emotions, 
+			R.drawable.clothes };
 	
 }
 
