1e1e9da647db7c6c0101345aa4faf7b945c2ac2d
MarijaNikolova <marijanikolova93@hotmail.com>
2015-07-30 11:33:39 +0200

    how many objects

---
+++ WeAreAllTheSame/src/weareallthesame/view/games/howmanygame/HowManyObjectsActivity.java
@@ -0,0 +1,21 @@
+package weareallthesame.view.games.howmanygame;
+
+import weareallthesame.view.R;
+import weareallthesame.view.R.id;
+import weareallthesame.view.R.layout;
+import weareallthesame.view.R.menu;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class HowManyObjectsActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_how_many_objects);
+	}
+
+	
+}
---
+++ WeAreAllTheSame/src/weareallthesame/view/games/howmanygame/HowManyObjectsView.java
@@ -0,0 +1,71 @@
+package weareallthesame.view.games.howmanygame;
+
+import java.util.ArrayList;
+import java.util.Set;
+
+import weareallthesame.model.items.Item;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class HowManyObjectsView extends View implements HowManyViewInterface{
+
+	private ArrayList<Item> items;
+	private Paint paint;
+	
+	public HowManyObjectsView(Context context) {
+		super(context);
+		// TODO Auto-generated constructor stub
+		items=new ArrayList<Item>();
+		paint=new Paint();
+	}
+	
+	public HowManyObjectsView(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		// TODO Auto-generated constructor stub
+		items=new ArrayList<Item>();
+		paint=new Paint();
+		
+	}
+	@Override
+	protected void onDraw(Canvas canvas) {
+		// TODO Auto-generated method stub
+		super.onDraw(canvas);
+		canvas.drawColor(Color.WHITE);
+		
+	}
+	
+	
+	@Override
+	public void setAnswer(Item item, int howMany) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setOfferedAnswers(Set<Integer> offeredAnswers) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void gameOver() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void wrongAnswer() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+
+
+	
+	
+	
+}
