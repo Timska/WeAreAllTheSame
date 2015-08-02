121aae11b10ddc345d45eff8e914010dc82f5d39
MarijaNikolova <marijanikolova93@hotmail.com>
2015-08-02 14:36:51 +0200

    my_activity.xml

--- WeAreAllTheSame/src/weareallthesame/view/MyActivity.java
+++ WeAreAllTheSame/src/weareallthesame/view/MyActivity.java
@@ -21,7 +21,7 @@ public class MyActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		setContentView(R.layout.activity_my);
 		
 		
 		b=(Button) findViewById(R.id.button1);
