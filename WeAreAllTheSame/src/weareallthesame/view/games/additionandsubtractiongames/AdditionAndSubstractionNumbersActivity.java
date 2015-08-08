c47a3ec14b06345f91ed37b46eab0141047c9987
MarijaNikolova <marijanikolova93@hotmail.com>
2015-08-08 15:04:17 +0200

    Addition and substraction numbers finished

--- WeAreAllTheSame/src/weareallthesame/view/games/additionandsubtractiongames/AdditionAndSubstractionNumbersActivity.java
+++ WeAreAllTheSame/src/weareallthesame/view/games/additionandsubtractiongames/AdditionAndSubstractionNumbersActivity.java
@@ -1,6 +1,7 @@
 package weareallthesame.view.games.additionandsubtractiongames;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Random;
 
 import weareallthesame.view.R;
@@ -8,15 +9,26 @@ import weareallthesame.view.R.id;
 import weareallthesame.view.R.layout;
 import weareallthesame.view.R.menu;
 import android.app.Activity;
+import android.app.ActionBar.LayoutParams;
+import android.content.ClipData;
+import android.content.ClipDescription;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Rect;
 import android.graphics.drawable.GradientDrawable;
+import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
+import android.view.DragEvent;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.View.DragShadowBuilder;
+import android.view.View.OnDragListener;
+import android.view.View.OnLongClickListener;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
@@ -56,6 +68,12 @@ public class AdditionAndSubstractionNumbersActivity extends Activity {
 		height = displayMetrics.heightPixels;
 
 		
+
+		mMediaPlayer = new MediaPlayer();
+		mMediaPlayer = MediaPlayer.create(this, R.raw.slow_whoop_bubble_pop);
+		mMediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+		
+		
 		answers=new ArrayList<TextView>();
 		boundsRect = new ArrayList<Rect>();
 		numbersAndSigns = new ArrayList<TextView>();
@@ -74,14 +92,19 @@ public class AdditionAndSubstractionNumbersActivity extends Activity {
 		answers.add((TextView) findViewById(R.id.addition_and_substraction_numbers_answer_three));
 		answers.add((TextView) findViewById(R.id.addition_and_substraction_numbers_answer_four));
 		
-		
+		colors = generateColors();
 		
 		
 		setAnswers();
-		colors = generateColors();
+		
 		setTextViews();
 		addAnswers();
 		
+		for(TextView tx:answers){
+			tx.setOnLongClickListener(new MyClickListener());
+		}
+		
+		numbersAndSigns.get(4).setOnDragListener(new MyDragListener());
 		
 		
 	}
@@ -93,35 +116,16 @@ public class AdditionAndSubstractionNumbersActivity extends Activity {
 			tx.setBackground(getGradientDrawable());
 			tx.setHeight(height/10);
 			tx.setText(answersString.get(i));
+			tx.setTag(answersString.get(i));
 			
 			
 		}
 
 	}
 
-	private void setButton() {
+	
 
-		int w = width / 3;
-		int h = height / 4;
-		playButton.setX(w);
-		playButton.setY(h);
-		Rect rect = new Rect(w, h, w + 200, h + 200);
-		boundsRect.add(rect);
-	}
-
-	private boolean isColiding(int x, int y, int width, int height) {
-
-		for (Rect bounds : boundsRect) {
-
-			if (bounds.left < x + width && bounds.left + bounds.width() > x
-					&& bounds.top < y + height
-					&& bounds.height() + bounds.top > y) {
-				return true;
-			}
-		}
-		return false;
-	}
-
+	
 	private ArrayList<Integer> generateColors() {
 
 		ArrayList<Integer> colors = new ArrayList<Integer>();
@@ -142,7 +146,7 @@ public class AdditionAndSubstractionNumbersActivity extends Activity {
 		answersString.add("+");
 		answersString.add("4");
 		answersString.add("=");
-		answersString.add("9");
+		answersString.add("");
 
 	}
 
@@ -171,8 +175,79 @@ public class AdditionAndSubstractionNumbersActivity extends Activity {
 			tx.setWidth(width/5);
 			tx.setHeight(height/10);
 			tx.setText("18");
+			tx.setTag(answersString.get(i));
 		}
+		answers.get(2).setTag("Correct");
+		Collections.shuffle(answers);
+		
 	}
 
+	private final class MyClickListener implements OnLongClickListener {
 
+		@Override
+		public boolean onLongClick(View v) {
+			// TODO Auto-generated method stub
+			ClipData.Item item = new ClipData.Item((CharSequence) v.getTag());
+			String[] mimeTypes = { ClipDescription.MIMETYPE_TEXT_PLAIN };
+			System.out.println(item.toString());
+			ClipData data = new ClipData(v.getTag().toString(), mimeTypes, item);
+			DragShadowBuilder dsb = new View.DragShadowBuilder(v);
+
+			v.startDrag(data, dsb, v, 0);
+			v.setVisibility(View.INVISIBLE);
+
+			return true;
+		}
+
+	}
+	private final class MyDragListener implements OnDragListener {
+
+		@Override
+		public boolean onDrag(View receivingLayoutView, DragEvent event) {
+			// TODO Auto-generated method stub
+
+			View draggedTextView = (View) event.getLocalState();
+
+			switch (event.getAction()) {
+			case DragEvent.ACTION_DRAG_STARTED:
+				break;
+			case DragEvent.ACTION_DRAG_ENTERED:
+				receivingLayoutView.setBackground(getGradientDrawable());
+				break;
+			case DragEvent.ACTION_DRAG_LOCATION:
+				break;
+			case DragEvent.ACTION_DROP:
+				
+				String tag=(String) draggedTextView.getTag();
+				
+				if(tag.equals("Correct")){
+					ViewGroup draggedImageViewParentLayout = (ViewGroup) draggedTextView
+							.getParent();
+					draggedImageViewParentLayout.removeView(draggedTextView);
+					TextView dropTarget=(TextView) receivingLayoutView;
+					TextView droppedView=(TextView) draggedTextView;
+					dropTarget.setText(droppedView.getText());
+					mMediaPlayer.start();
+					draggedTextView.setVisibility(View.VISIBLE);
+					return true;
+				}
+				else{
+					draggedTextView.setVisibility(View.VISIBLE);
+					return false;
+				}
+			
+			case DragEvent.ACTION_DRAG_ENDED:
+
+				if (!event.getResult()) {
+
+					draggedTextView.setVisibility(View.VISIBLE);
+				}
+			default:
+				break;
+			}
+			return true;
+		}
+
+	}
+	
 }
