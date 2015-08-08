1646bb3986a3a6ce840ccec5c4a7ec0e4e565fd4
MarijaNikolova <marijanikolova93@hotmail.com>
2015-08-08 12:18:50 +0200

    how many objects

--- WeAreAllTheSame/src/weareallthesame/view/games/howmanygame/HowManyObjectsActivity.java
+++ WeAreAllTheSame/src/weareallthesame/view/games/howmanygame/HowManyObjectsActivity.java
@@ -1,16 +1,108 @@
 package weareallthesame.view.games.howmanygame;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 import weareallthesame.view.R;
 import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Rect;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 public class HowManyObjectsActivity extends Activity {
+
+	private ArrayList<ImageView> objects;
+	private ArrayList<Rect> rectBounds;
+	private Random r=new Random();
+	private DisplayMetrics displayMetrics;
+	private int width,height;
+	private LinearLayout container;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_how_many_objects);
-	}
 
+		/*displayMetrics = new DisplayMetrics();
+		getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+		width = displayMetrics.widthPixels;
+		height = displayMetrics.heightPixels; */
+		
+		
+		/*objects = new ArrayList<ImageView>();
+		//fillObjects();
+		rectBounds=new ArrayList<Rect>();
+		//addImageViews();
+		
+		//container = (LinearLayout) findViewById(R.id.how_many_objects_container);
+		for(int i=0;i<objects.size();++i){
+			//container.addView(objects.get(i));
+		}
+		*/
+		
+		
+
+	}
 	
+	/*private void addImageViews(){
+		
+		int i=0;
+		while(rectBounds.size()<objects.size()){
+			
+			int x=r.nextInt(width/2);
+			int y=r.nextInt(height/3);
+			
+			int w=200;
+			int h=200;
+			
+			if(!isColiding(x, y, w, h)){
+				rectBounds.add(new Rect(x,y,x+w,y+h));
+				objects.get(i).setX(x);
+				objects.get(i).setY(y);
+				++i;
+			}
+			
+			
+		}
+		
+		
+		
+		
+		
+	}
+	
+
+	private void fillObjects(){
+		
+		int n=r.nextInt(10)+1;
+		int i=0;
+		while(i<n){
+			ImageView img=new ImageView(this);
+			Bitmap b=Bitmap.createScaledBitmap(BitmapFactory.decodeResource(getResources(), R.drawable.piperka), 100, 100,true);
+	
+			//img.setBackgroundResource(b);
+			objects.add(img);
+		}
+	
+		
+		
+	}
+	private boolean isColiding(int x, int y, int width, int height) {
+
+		for (Rect bounds : rectBounds) {
+
+			if (bounds.left < x + width && bounds.left + bounds.width() > x
+					&& bounds.top < y + height
+					&& bounds.height() + bounds.top > y) {
+				return true;
+			}
+		}
+		return false;
+	}
+	*/
 }
--- WeAreAllTheSame/src/weareallthesame/view/games/howmanygame/HowManyObjectsView.java
+++ WeAreAllTheSame/src/weareallthesame/view/games/howmanygame/HowManyObjectsView.java
@@ -1,73 +1,165 @@
 package weareallthesame.view.games.howmanygame;
 
 import java.util.ArrayList;
+import java.util.Random;
 import java.util.Set;
 
 import weareallthesame.model.items.Item;
+import weareallthesame.view.R;
+import android.R.color;
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Rect;
+import android.graphics.PorterDuff.Mode;
 import android.util.AttributeSet;
+import android.util.DisplayMetrics;
+import android.view.SurfaceHolder;
 import android.view.View;
 
-public class HowManyObjectsView extends View implements HowManyViewInterface{
+public class HowManyObjectsView extends View implements HowManyViewInterface {
 
 	private static final long serialVersionUID = 8469603708744698511L;
 
-	private ArrayList<Item> items;
-	private Paint paint;
-	
+	private ArrayList<Bitmap> bitmaps;
+	private ArrayList<Rect> boundsBitmaps;
+	private Random r = new Random();
+	private DisplayMetrics displayMetrics;
+	private int vWidth = 500, vHeight = 300;
+	private Rect bounds;
+	private Paint paint = new Paint();
+	private SurfaceHolder surfaceHolder;
+
 	public HowManyObjectsView(Context context) {
 		super(context);
 		// TODO Auto-generated constructor stub
-		items=new ArrayList<Item>();
-		paint=new Paint();
+		bitmaps = new ArrayList<Bitmap>();
+		boundsBitmaps = new ArrayList<Rect>();
+
+		addScaledBitmaps();
+		addBitmaps();
+		paint.setStyle(Paint.Style.STROKE);
 	}
-	
+
 	public HowManyObjectsView(Context context, AttributeSet attrs) {
 		super(context, attrs);
 		// TODO Auto-generated constructor stub
-		items=new ArrayList<Item>();
-		paint=new Paint();
-		
+		bitmaps = new ArrayList<Bitmap>();
+		boundsBitmaps = new ArrayList<Rect>();
+
+		addScaledBitmaps();
+		addBitmaps();
+		paint.setStyle(Paint.Style.STROKE);
+
 	}
+
 	@Override
 	protected void onDraw(Canvas canvas) {
 		// TODO Auto-generated method stub
+
 		super.onDraw(canvas);
-		canvas.drawColor(Color.WHITE);
+		//canvas.onDraw(canvas);
+		//canvas.drawColor(0, Mode.CLEAR);
+		//canvas.drawColor(color.holo_red_light);
+		for (int i = 0; i < bitmaps.size(); ++i) {
+			bounds = boundsBitmaps.get(i);
+			canvas.drawBitmap(bitmaps.get(i), bounds.left, bounds.top, paint);
+		}
 		
 	}
-	
-	
+
+	public int c = 0; // c is a counter of frames
+
+	/*@Override
+	public void draw(Canvas canvas) {
+		c++;
+		if (c >= 2 && c <= 4) { // change it to your own condition, the goal is
+								// to draw it only two times(2 passes), every
+								// time you need to "refresh" the screen
+			canvas.drawColor(Color.TRANSPARENT, Mode.CLEAR);
+		}
+	} */
+
+	private void drawSurface(Canvas canvas) {
+
+		canvas.drawColor(color.holo_red_light, Mode.CLEAR);
+		for (int i = 0; i < bitmaps.size(); ++i) {
+			bounds = boundsBitmaps.get(i);
+			canvas.drawBitmap(bitmaps.get(i), bounds.left, bounds.top, paint);
+		}
+
+	}
+
+	private boolean isColiding(int x, int y, int width, int height) {
+
+		for (Rect bounds : boundsBitmaps) {
+
+			if (bounds.left < x + width && bounds.left + bounds.width() > x
+					&& bounds.top < y + height
+					&& bounds.height() + bounds.top > y) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	private void addBitmaps() {
+
+		int i = 0;
+		while (boundsBitmaps.size() < bitmaps.size()) {
+
+			int x = r.nextInt(vWidth - 110);
+			int y = r.nextInt(vHeight - 110);
+
+			if (!isColiding(x, y, 110, 110)) {
+
+				bounds = new Rect(x, y, x + 110, y + 110);
+				boundsBitmaps.add(bounds);
+
+				++i;
+			}
+
+		}
+
+	}
+
+	private void addScaledBitmaps() {
+
+		int n = r.nextInt(10) + 1;
+		Bitmap b = Bitmap.createScaledBitmap(BitmapFactory.decodeResource(
+				getResources(), R.drawable.piperka), 100, 100, true);
+
+		for (int i = 0; i < n; ++i) {
+			bitmaps.add(b);
+		}
+
+	}
+
 	@Override
 	public void setAnswer(Item item, int howMany) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void setOfferedAnswers(Set<Integer> offeredAnswers) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void gameOver() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void wrongAnswer() {
 		// TODO Auto-generated method stub
-		
+
 	}
-	
 
-
-	
-	
-	
 }
