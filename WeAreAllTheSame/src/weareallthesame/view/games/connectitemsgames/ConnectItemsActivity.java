6cd420bca915d0debdd96e927d1f27e77a1a5f4b
MarijaNikolova <marijanikolova93@hotmail.com>
2015-08-09 14:55:08 +0200

    hangman design finished. connect items design almost finished.

--- WeAreAllTheSame/src/weareallthesame/view/games/connectitemsgames/ConnectItemsActivity.java
+++ WeAreAllTheSame/src/weareallthesame/view/games/connectitemsgames/ConnectItemsActivity.java
@@ -1,4 +1,4 @@
-package weareallthesame.view.games.hangmangame;
+package weareallthesame.view.games.connectitemsgames;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -10,7 +10,6 @@ import android.app.Activity;
 import android.graphics.Color;
 import android.graphics.Paint.Align;
 import android.graphics.drawable.GradientDrawable;
-import android.graphics.drawable.shapes.Shape;
 import android.os.Bundle;
 import android.text.style.TextAppearanceSpan;
 import android.util.DisplayMetrics;
@@ -18,13 +17,12 @@ import android.view.Gravity;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
-public class HangmanGameActivity extends Activity implements
-		HangmanViewInterface {
+public class ConnectItemsActivity extends Activity {
 
 	private static final long serialVersionUID = 6373187583074782521L;
 
 	private ArrayList<TextView> listLetters;
-	private ArrayList<TextView> listSpaces;
+	private ArrayList<TextView> listImages;
 	private ArrayList<Integer> colors;
 	private LinearLayout layoutLetters;
 	private LinearLayout layoutSpaces;
@@ -37,7 +35,7 @@ public class HangmanGameActivity extends Activity implements
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_hangman_game);
+		setContentView(R.layout.activity_connect_items);
 
 		displayMetrics = new DisplayMetrics();
 		getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
@@ -45,99 +43,69 @@ public class HangmanGameActivity extends Activity implements
 		height = displayMetrics.heightPixels;
 
 		listLetters = new ArrayList<TextView>();
-		listSpaces = new ArrayList<TextView>();
+		listImages = new ArrayList<TextView>();
 		colors = generateColors();
 
 		setTextViews(word, spaces);
 
-		
-		layoutSpaces = (LinearLayout) findViewById(R.id.hangman_game_layout_spaces);
-		layoutLetters = (LinearLayout) findViewById(R.id.hangman_game_layout_letters);
+		layoutLetters = (LinearLayout) findViewById(R.id.connect_items_layout_letters);
+		layoutSpaces = (LinearLayout) findViewById(R.id.connect_items_layout_images);
+
+		LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(
+				LinearLayout.LayoutParams.MATCH_PARENT,
+				LinearLayout.LayoutParams.WRAP_CONTENT);
+
+		layoutParams.setMargins(width / 7, 10, width / 7, 0);
+		layoutParams.gravity = Gravity.CENTER;
 
 		LinearLayout.LayoutParams layoutParamsSpaces = new LinearLayout.LayoutParams(
-				LinearLayout.LayoutParams.WRAP_CONTENT,
+				LinearLayout.LayoutParams.MATCH_PARENT,
 				LinearLayout.LayoutParams.WRAP_CONTENT);
-		layoutSpaces.setOrientation(LinearLayout.VERTICAL);
-		layoutParamsSpaces.setMargins(0, 5, 0, 5);
+		layoutParams.setMargins(width / 7, 10, width / 7, 0);
 
-		LinearLayout.LayoutParams layoutParamsLetters = new LinearLayout.LayoutParams(
-				LinearLayout.LayoutParams.WRAP_CONTENT,
-				LinearLayout.LayoutParams.WRAP_CONTENT);
-		layoutParamsLetters.setMargins(5, 0, 5, 0);
-		//layoutParamsLetters.gravity = Gravity.CENTER | Gravity.BOTTOM;
+		layoutParamsSpaces.setMargins(0, 10, width / 7, 0);
 
 		for (int i = 0; i < listLetters.size(); ++i) {
-			layoutSpaces.addView(listSpaces.get(i), layoutParamsSpaces);
-			layoutLetters.addView(listLetters.get(i), layoutParamsLetters);
-
+			layoutLetters.addView(listLetters.get(i), layoutParams);
+			layoutSpaces.addView(listImages.get(i), layoutParamsSpaces);
 		}
 
 	}
 
-	@Override
-	public void gameOver() {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void setOrUpdateOfferedLettersAndUsedLetters(
-			List<Character> allOfferedLetters, List<Boolean> usedLettersFlagged) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void setOrUpdateUserAnswer(List<Character> userAnswer) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void setAnswer(Item item) {
-		// TODO Auto-generated method stub
-
-	}
+	
 
 	private void setTextViews(String word, String spaces) {
 
 		for (int i = 0; i < word.length(); ++i) {
 
-			TextView txLetter = new TextView(getApplicationContext());
-	
+			TextView txL = new TextView(getApplicationContext());
 			TextView txS = new TextView(getApplicationContext());
 
+			txL.setBackground(getGradientDrawable());
 			txS.setBackground(getGradientDrawable());
-			GradientDrawable gd=getGradientDrawable();
-			gd.setShape(GradientDrawable.OVAL);
-			txLetter.setBackground(gd);
 
-			txLetter.setText(word.charAt(i) + " ");
+			txL.setText(word.charAt(i) + " ");
 			txS.setText(spaces.charAt(i) + " ");
 
-			txLetter.setTextAppearance(getApplicationContext(),
+			txL.setTextAppearance(getApplicationContext(),
 					android.R.style.TextAppearance_Large);
 			txS.setTextAppearance(getApplicationContext(),
 					android.R.style.TextAppearance_Large);
 
-			txLetter.setGravity(Gravity.CENTER);
+			txL.setGravity(Gravity.CENTER);
 			txS.setGravity(Gravity.CENTER);
 
-			txLetter.setTextColor(Color.BLACK);
+			txL.setTextColor(Color.BLACK);
 			txS.setTextColor(Color.BLACK);
 
-			txLetter.setWidth(width / (spaces.length() + 1));
-			txS.setWidth(width / 3);
+			txL.setWidth(width / 4);
+			txS.setWidth(width / 4);
 
-			txLetter.setHeight(height / 10);
+			txL.setHeight(height / 10);
 			txS.setHeight(height / 10);
-			
 
-			listLetters.add(txLetter);
-			listSpaces.add(txS);
-		}
-		for(int k=0;k<listLetters.size();++k){
-			System.out.println(listSpaces.get(k).getText());
+			listLetters.add(txL);
+			listImages.add(txS);
 		}
 
 	}
@@ -162,7 +130,7 @@ public class HangmanGameActivity extends Activity implements
 		GradientDrawable gd = new GradientDrawable();
 		gd.setColor(i);
 		gd.setCornerRadius(10);
-		gd.setShape(GradientDrawable.RECTANGLE);
+		gd.setShape(GradientDrawable.OVAL);
 		// gd.setStroke(1, 0xFF000000);
 		gd.setStroke(2, Color.BLACK, 5, 5);
 
