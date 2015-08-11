b29e1c3421be0720f52a65fa841a787c39a576be
MarijaNikolova <marijanikolova93@hotmail.com>
2015-08-11 17:45:27 +0200

    question game finished

--- WeAreAllTheSame/src/weareallthesame/view/games/questiongame/QuestionGameActivity.java
+++ WeAreAllTheSame/src/weareallthesame/view/games/questiongame/QuestionGameActivity.java
@@ -5,11 +5,21 @@ import java.util.Random;
 
 import weareallthesame.view.R;
 import android.app.Activity;
+import android.content.ClipData;
+import android.content.ClipDescription;
 import android.graphics.Color;
 import android.graphics.drawable.GradientDrawable;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
+import android.view.DragEvent;
 import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.View.DragShadowBuilder;
+import android.view.View.OnDragListener;
+import android.view.View.OnLongClickListener;
 import android.widget.TextView;
 
 public class QuestionGameActivity extends Activity {
@@ -17,10 +27,12 @@ public class QuestionGameActivity extends Activity {
 	private ArrayList<TextView> txtAnswers;
 	private ArrayList<Integer> colors;
 	private TextView txtQuestion;
+	private TextView txtAnswer;
 	private String guestion;
 	private Random r = new Random();
 	private DisplayMetrics displayMetrics;
 	private int width, height,w;
+	private MediaPlayer mMediaPlayer;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -32,8 +44,11 @@ public class QuestionGameActivity extends Activity {
 		width = displayMetrics.widthPixels;
 		height = displayMetrics.heightPixels;
 		w=width/4;
+		mMediaPlayer = new MediaPlayer();
+		mMediaPlayer = MediaPlayer.create(this, R.raw.slow_whoop_bubble_pop);
+		mMediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
 		
-
+		txtAnswer=(TextView) findViewById(R.id.question_game_answer);
 		txtAnswers = new ArrayList<TextView>();
 		txtAnswers.add((TextView) findViewById(R.id.question_game_answer_one));
 		txtAnswers.add((TextView) findViewById(R.id.question_game_answer_two));
@@ -46,6 +61,13 @@ public class QuestionGameActivity extends Activity {
 		txtQuestion.setText("Kako se vika bratot na Marija?");
 		txtQuestion.setGravity(Gravity.CENTER);
 		setAnswers();
+		
+		txtAnswer.setBackground(getGradientDrawable());
+		txtAnswer.setOnDragListener(new MyDragListener());
+		
+		for(int i=0;i<txtAnswers.size();++i){
+			txtAnswers.get(i).setOnLongClickListener(new MyClickListener());
+		}
 
 	}
 
@@ -55,11 +77,12 @@ public class QuestionGameActivity extends Activity {
 
 			tx.setBackground(getGradientDrawable());
 			tx.setText("Martin");
-			//tx.setX(20);
+			tx.setTag("Incorrect");
 			tx.setWidth(w);
 			w+=(width/4);
 
 		}
+		txtAnswers.get(0).setTag("Correct");
 	}
 
 	private ArrayList<Integer> generateColors() {
@@ -89,5 +112,71 @@ public class QuestionGameActivity extends Activity {
 		return gd;
 
 	}
+	private final class MyClickListener implements OnLongClickListener {
 
+		@Override
+		public boolean onLongClick(View v) {
+			// TODO Auto-generated method stub
+			ClipData.Item item = new ClipData.Item((CharSequence) v.getTag());
+			String[] mimeTypes = { ClipDescription.MIMETYPE_TEXT_PLAIN };
+			System.out.println(item.toString());
+			ClipData data = new ClipData(v.getTag().toString(), mimeTypes, item);
+			DragShadowBuilder dsb = new View.DragShadowBuilder(v);
+
+			v.startDrag(data, dsb, v, 0);
+			v.setVisibility(View.INVISIBLE);
+
+			return true;
+		}
+
+	}
+
+	private final class MyDragListener implements OnDragListener {
+
+		@Override
+		public boolean onDrag(View receivingLayoutView, DragEvent event) {
+			// TODO Auto-generated method stub
+
+			View draggedTextView = (View) event.getLocalState();
+
+			switch (event.getAction()) {
+			case DragEvent.ACTION_DRAG_STARTED:
+				break;
+			case DragEvent.ACTION_DRAG_ENTERED:
+				receivingLayoutView.setBackground(getGradientDrawable());
+				break;
+			case DragEvent.ACTION_DRAG_LOCATION:
+				break;
+			case DragEvent.ACTION_DROP:
+
+				String tag = (String) draggedTextView.getTag();
+
+				if (tag.equals("Correct")) {
+					ViewGroup draggedImageViewParentLayout = (ViewGroup) draggedTextView
+							.getParent();
+					draggedImageViewParentLayout.removeView(draggedTextView);
+					TextView dropTarget = (TextView) receivingLayoutView;
+					TextView droppedView = (TextView) draggedTextView;
+					dropTarget.setText(droppedView.getText());
+					mMediaPlayer.start();
+					draggedTextView.setVisibility(View.VISIBLE);
+					return true;
+				} else {
+					draggedTextView.setVisibility(View.VISIBLE);
+					return false;
+				}
+
+			case DragEvent.ACTION_DRAG_ENDED:
+
+				if (!event.getResult()) {
+
+					draggedTextView.setVisibility(View.VISIBLE);
+				}
+			default:
+				break;
+			}
+			return true;
+		}
+
+	}
 }
