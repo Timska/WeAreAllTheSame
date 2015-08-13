923a493b83e7f9272fd293fcb47a31f533dbcbd6
MarijaNikolova <marijanikolova93@hotmail.com>
2015-08-13 15:43:02 +0200

    connect items activity. almost ready.

--- WeAreAllTheSame/src/weareallthesame/view/games/orderelementsgame/OrderElementsActivity.java
+++ WeAreAllTheSame/src/weareallthesame/view/games/orderelementsgame/OrderElementsActivity.java
@@ -7,7 +7,11 @@ import weareallthesame.view.R;
 import android.app.Activity;
 import android.content.ClipData;
 import android.content.ClipDescription;
+import android.graphics.Color;
+import android.graphics.drawable.GradientDrawable;
+import android.graphics.drawable.GradientDrawable.Orientation;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.view.DragEvent;
 import android.view.Gravity;
 import android.view.View;
@@ -23,44 +27,87 @@ public class OrderElementsActivity extends Activity {
 	private ArrayList<String> answers;
 	private ArrayList<TextView> txtAnswers;
 	private LinearLayout container;
-	
+	private DisplayMetrics displayMetrics;
+	private int width,height;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_order_elements);
+
+		displayMetrics = new DisplayMetrics();
+		getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+		width = displayMetrics.widthPixels;
+		height = displayMetrics.heightPixels;
 		
-		answers=new ArrayList<String>();
+		
+		answers = new ArrayList<String>();
 		answers.add("igor");
 		answers.add("zorica");
 		answers.add("marija");
 		answers.add("aleksandra");
 		answers.add("martin");
-		
-		txtAnswers=new ArrayList<TextView>();
-		container=(LinearLayout) findViewById(R.id.order_elements_container);
-		for(int i=0;i<answers.size();++i){
-			TextView tx=new TextView(getApplicationContext());
-			tx.setText(answers.get(i));
-			tx.setWidth(140);
-			tx.setGravity(Gravity.CENTER);
-			tx.setTag(String.format("%d", i));
-		//	tx.setBackgroundResource(R.drawable.play_button);
-			txtAnswers.add(tx);
-			
-			
-		}
-		Collections.shuffle(txtAnswers);
-		for(int i=0;i<answers.size();++i){
-			String tag=(String) txtAnswers.get(i).getTag();
-			tag+=" "+i;
+
+		txtAnswers = new ArrayList<TextView>();
+
+		container = (LinearLayout) findViewById(R.id.order_elements_container);
+		LinearLayout.LayoutParams layoutParams= new LinearLayout.LayoutParams(
+				LinearLayout.LayoutParams.MATCH_PARENT,
+				LinearLayout.LayoutParams.WRAP_CONTENT);
+		container.setOrientation(LinearLayout.VERTICAL);
+		layoutParams.setMargins(0, 5, 0, 5);
+
+		setTextViews();
+		addViewsTagsAndListeners(layoutParams);
+
+	}
+
+	private void addViewsTagsAndListeners(LinearLayout.LayoutParams layPar) {
+		for (int i = 0; i < answers.size(); ++i) {
+			String tag = (String) txtAnswers.get(i).getTag();
+			tag += " " + i;
 			txtAnswers.get(i).setTag(tag);
-			container.addView(txtAnswers.get(i));
-			//System.out.println(txtAnswers.get(i).getTag());
+			container.addView(txtAnswers.get(i),layPar);
 			txtAnswers.get(i).setOnLongClickListener(new MyClickListener());
 			txtAnswers.get(i).setOnDragListener(new MyDragListener());
 		}
+		
 	}
 
+	private void setTextViews() {
+		for (int i = 0; i < answers.size(); ++i) {
+			TextView tx = new TextView(getApplicationContext());
+			tx.setText(answers.get(i));
+			tx.setHeight(height/(answers.size()+2));
+			tx.setGravity(Gravity.CENTER);
+			tx.setTag(String.format("%d", i));
+			tx.setTextColor(Color.BLACK);
+			tx.setBackground(getGradientDrawable());
+
+			txtAnswers.add(tx);
+
+		}
+		Collections.shuffle(txtAnswers);
+	}
+
+	private GradientDrawable getGradientDrawable() {
+
+		// int i = colors.get(r.nextInt(colors.size()));
+		int[] colorsInt = { Color.rgb(83, 249, 94), Color.rgb(255, 255, 102) };
+
+		// int i = colors.get(r.nextInt(colors.size()));
+		GradientDrawable gd = new GradientDrawable();
+		gd.setOrientation(Orientation.LEFT_RIGHT);
+		gd.setColors(colorsInt);
+		// gd.setColor(i);
+		gd.setCornerRadius(10);
+		gd.setShape(GradientDrawable.RECTANGLE);
+		// gd.setStroke(1, 0xFF000000);
+		gd.setStroke(2, Color.BLACK, 5, 5);
+
+		return gd;
+
+	}
 
 	private final class MyClickListener implements OnLongClickListener {
 
@@ -81,12 +128,8 @@ public class OrderElementsActivity extends Activity {
 
 	}
 
-	
-
 	private final class MyDragListener implements OnDragListener {
 
-		
-		
 		@Override
 		public boolean onDrag(View receivingLayoutView, DragEvent event) {
 			// TODO Auto-generated method stub
@@ -103,35 +146,25 @@ public class OrderElementsActivity extends Activity {
 			case DragEvent.ACTION_DROP:
 
 				String tag = (String) draggedTextView.getTag();
-				String [] tagsDraggedTextView=tag.split(" ");
-				String [] tagsReceivingTextView=((String) receivingLayoutView.getTag()).split(" ");
+				String[] tagsDraggedTextView = tag.split(" ");
+				String[] tagsReceivingTextView = ((String) receivingLayoutView
+						.getTag()).split(" ");
 
-				
 				if (tagsDraggedTextView[0].equals(tagsReceivingTextView[1])) {
-					System.out.println("Dropped");
-					System.out.println("Dragged text view"+tagsDraggedTextView[0]+" "+tagsDraggedTextView[1]);
-					System.out.println("Dropping text view" + tagsReceivingTextView[0]+" "+tagsReceivingTextView[1]);
-					ViewGroup draggedImageViewParentLayout = (ViewGroup) draggedTextView
-							.getParent();
-					//draggedImageViewParentLayout.removeView(draggedTextView);
+
 					TextView dropTarget = (TextView) receivingLayoutView;
 					TextView draggedView = (TextView) draggedTextView;
-					String targetText=(String) dropTarget.getText();
+					String targetText = (String) dropTarget.getText();
 					dropTarget.setText(draggedView.getText());
 					draggedView.setText(targetText);
-					draggedView.setTag(tagsReceivingTextView[0]+" "+tagsDraggedTextView[1]);
-					dropTarget.setTag(tagsDraggedTextView[0] +" "+ tagsReceivingTextView[1]);
-					System.out.println("Changed to>");
-					System.out.println("Dragged text view"+draggedTextView.getTag());
-					System.out.println("Dropping text view" + dropTarget.getTag());
-					//int indexDropTarget=txtAnswers.indexOf(drop)
+					draggedView.setTag(tagsReceivingTextView[0] + " "
+							+ tagsDraggedTextView[1]);
+					dropTarget.setTag(tagsDraggedTextView[0] + " "
+							+ tagsReceivingTextView[1]);
 					draggedTextView.setVisibility(View.VISIBLE);
 					return true;
 				} else {
 					draggedTextView.setVisibility(View.VISIBLE);
-					System.out.println("not dropped");
-					System.out.println("Dragged text view"+tagsDraggedTextView[0]+" "+tagsDraggedTextView[1]);
-					System.out.println("Dropping text view" + tagsReceivingTextView[0]+" "+tagsReceivingTextView[1]);
 					return false;
 				}
 
