2060396077222f93597e0441fbb9416c3c4669ee
MarijaNikolova <marijanikolova93@hotmail.com>
2015-08-24 10:36:04 +0200

    hangman standard

--- WeAreAllTheSame/src/weareallthesame/view/games/hangmangame/HangmanGameStandardActivity.java
+++ WeAreAllTheSame/src/weareallthesame/view/games/hangmangame/HangmanGameStandardActivity.java
@@ -1,38 +1,81 @@
 package weareallthesame.view.games.hangmangame;
 
+import java.util.ArrayList;
+
 import weareallthesame.view.R;
-import weareallthesame.view.R.id;
-import weareallthesame.view.R.layout;
-import weareallthesame.view.R.menu;
 import android.app.Activity;
+import android.graphics.Color;
+import android.graphics.Typeface;
+import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.util.DisplayMetrics;
+import android.view.Gravity;
+import android.widget.LinearLayout;
+import android.widget.LinearLayout.LayoutParams;
+import android.widget.TextView;
 
 public class HangmanGameStandardActivity extends Activity {
+
+	private LinearLayout wordWithEmptySpaces;
+	private ArrayList<TextView> txtWordWithEmptySpaces;
+	private String word = "M_r_ja";
+	private DisplayMetrics displayMetrics;
+	private int width, height;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_hangman_game_standard);
+
+		displayMetrics = new DisplayMetrics();
+		getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+		width = displayMetrics.widthPixels;
+		height = displayMetrics.heightPixels;
+
+		wordWithEmptySpaces = (LinearLayout) findViewById(R.id.hangman_game_standard_layout_word_with_empty_letters);
+		LinearLayout.LayoutParams layParams = new LinearLayout.LayoutParams(
+				LinearLayout.LayoutParams.WRAP_CONTENT,
+				LinearLayout.LayoutParams.WRAP_CONTENT);
+		wordWithEmptySpaces.setOrientation(LinearLayout.VERTICAL);
+		layParams.setMargins(0, 5, 0, 5);
+
+		addTextViews(layParams);
+
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.hangman_game_standard, menu);
-		return true;
-	}
+	private void addTextViews(LayoutParams layParams) {
 
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
+		txtWordWithEmptySpaces = new ArrayList<TextView>();
+		for (int i = 0; i < word.length(); ++i) {
+			TextView tx = new TextView(getApplicationContext());
+			tx.setBackground(getGradientDrawable());
+			tx.setText(word.charAt(i) + " ");
+			tx.setWidth(width / 3);
+			tx.setHeight(height / 10);
+			tx.setGravity(Gravity.CENTER);
+			tx.setTextColor(Color.BLACK);
+			tx.setTextSize(50);
+
+			// Typeface
+			// tf=Typeface.createFromFile("assets/fonts/chochet_pattern.ttf");
+			// Typeface.createFromAsset(getAssets(),
+			// "fonts/chochet_pattern.ttf");
+			// tx.setTypeface(tf);
+			txtWordWithEmptySpaces.add(tx);
+			wordWithEmptySpaces.addView(tx, layParams);
 		}
-		return super.onOptionsItemSelected(item);
+
 	}
+
+	private GradientDrawable getGradientDrawable() {
+
+		GradientDrawable gd = new GradientDrawable();
+		gd.setColor(Color.rgb(225, 125, 105));
+		gd.setCornerRadius(10);
+		gd.setShape(GradientDrawable.RECTANGLE);
+		// gd.setStroke(1, 0xFF000000);
+		gd.setStroke(2, Color.BLACK, 5, 5);
+		return gd;
+	}
+
 }
