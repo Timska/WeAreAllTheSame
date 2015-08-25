502f99adfbc80688af054764151097147814ec2d
MarijaNikolova <marijanikolova93@hotmail.com>
2015-08-25 20:17:30 +0200

    choose character from sound modified

--- WeAreAllTheSame/src/weareallthesame/view/games/choosecharacterfromsoundgame/ChooseCharacterFromSoundActivity.java
+++ WeAreAllTheSame/src/weareallthesame/view/games/choosecharacterfromsoundgame/ChooseCharacterFromSoundActivity.java
@@ -36,155 +36,105 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 
 @SuppressLint("NewApi")
-public class ChooseCharacterFromSoundActivity extends Activity implements ChooseStringFromSoundViewInterface {
+public class ChooseCharacterFromSoundActivity extends Activity implements
+		ChooseStringFromSoundViewInterface {
 
+	public static final int TEXTVIEWCOLOR = Color.rgb(88, 243, 129);
 	private DisplayMetrics displayMetrics;
 	private ArrayList<Rect> boundsRect;
 	private ArrayList<TextView> answers;
-	private int width, height, rWidth, rHeight;
-	private ArrayList<Integer> colors;
+	private int width, height;
 	private ArrayList<String> answersString;
 	private MediaPlayer mMediaPlayer;
 	private Random r = new Random();
 	private Button playButton;
 	private Animation animation;
-	private LinearLayout linLayout;
 	private TextView dropPlace;
 	private ApplicationInterface appInterface;
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_choose_character_from_sound);
 
 		openGame();
-		displayMetrics = new DisplayMetrics();
-		getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
-		width = displayMetrics.widthPixels;
-		height = displayMetrics.heightPixels;
+		getMetrics();
 
 		boundsRect = new ArrayList<Rect>();
 		answers = new ArrayList<TextView>();
 		animation = AnimationUtils.loadAnimation(this,
 				R.anim.choose_character_from_sound_animation_scaling);
 
-		playButton = (Button) findViewById(R.id.choose_character_from_sound_play_buttons);
-		answers.add((TextView) findViewById(R.id.choose_character_from_sound_answer_one));
-		answers.add((TextView) findViewById(R.id.choose_character_from_sound_answer_two));
-		answers.add((TextView) findViewById(R.id.choose_character_from_sound_answer_three));
-		answers.add((TextView) findViewById(R.id.choose_character_from_sound_answer_four));
-		linLayout = (LinearLayout) findViewById(R.id.choose_character_from_sound_drop_pool);
+		initiliazeViews();
+		setButtonSound();
 
-		
-		setButton();
 		setAnswers();
-		colors = generateColors();
 		setTextViews();
-
-		mMediaPlayer = new MediaPlayer();
-		mMediaPlayer = MediaPlayer.create(this, R.raw.slow_whoop_bubble_pop);
-		mMediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
-		// mMediaPlayer.setLooping(true);
-		mMediaPlayer.start();
-
-		playButton.setOnClickListener(new OnClickListener() {
-
-			@Override
-			public void onClick(View v) {
-				// TODO Auto-generated method stub
-				mMediaPlayer.start();
-				boundsRect.clear();
-				setButton();
-				setTextViews();
-
-				playButton.startAnimation(animation);
-			}
-		});
 
 		for (int i = 0; i < answers.size(); ++i) {
 			answers.get(i).setTag(i + " ");
 			answers.get(i).setOnLongClickListener(new MyClickListener());
 		}
 
-		linLayout.setOnDragListener(new MyDragListener());
-		//dropPlace.setOnDragListener(new MyDragListener());
-		
+		// linLayout.setOnDragListener(new MyDragListener());
+		// dropPlace.setOnDragListener(new MyDragListener());
+
+	}
+
+	public void playButtonOnClick(View v) {
+		mMediaPlayer.start();
+		setTextViews();
+		playButton.startAnimation(animation);
+	}
+
+	private void setButtonSound() {
+
+		mMediaPlayer = new MediaPlayer();
+		mMediaPlayer = MediaPlayer.create(this, R.raw.slow_whoop_bubble_pop);
+		mMediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+		// mMediaPlayer.setLooping(true);
+		mMediaPlayer.start();
+	}
+
+	private void initiliazeViews() {
+		playButton = (Button) findViewById(R.id.choose_character_from_sound_play_buttons);
+		answers.add((TextView) findViewById(R.id.choose_character_from_sound_answer_one));
+		answers.add((TextView) findViewById(R.id.choose_character_from_sound_answer_two));
+		answers.add((TextView) findViewById(R.id.choose_character_from_sound_answer_three));
+		answers.add((TextView) findViewById(R.id.choose_character_from_sound_answer_four));
+		dropPlace = (TextView) findViewById(R.id.choose_character_from_sound_drop_pool);
+
+	}
+
+	private void getMetrics() {
+		displayMetrics = new DisplayMetrics();
+		getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+		width = displayMetrics.widthPixels;
+		height = displayMetrics.heightPixels;
 	}
 
 	private void openGame() {
 		// TODO Auto-generated method stub
-		Intent intent=getIntent();
-		String gameType=intent.getStringExtra("gameType");
-		ArrayList<String> gameTags=intent.getStringArrayListExtra("gameTags");
-		//intent.getSerializableExtra("");
-		//ne se sekjavam toa so serializable so trebase da se izvade od intentot
-		//appInterface.openGame(gameType, gameTags.iterator(), this, R.string.choose_character_from_sound_task_description);
-		
+		Intent intent = getIntent();
+		String gameType = intent.getStringExtra("gameType");
+		ArrayList<String> gameTags = intent.getStringArrayListExtra("gameTags");
+		// intent.getSerializableExtra("");
+		// ne se sekjavam toa so serializable so trebase da se izvade od
+		// intentot
+		// appInterface.openGame(gameType, gameTags.iterator(), this,
+		// R.string.choose_character_from_sound_task_description);
+
 	}
 
 	private void setTextViews() {
 
-		rHeight = width;
-		rWidth = height / 2;
-
-		int i = 0;
-
-		while (boundsRect.size() < answers.size() + 1) {
-
-			Paint paint = answers.get(i).getPaint();
+		for (int i = 0; i < answers.size(); ++i) {
+			answers.get(i).setWidth(width / 3);
+			answers.get(i).setHeight(height / 8);
+			answers.get(i).setBackground(getGradientDrawable(TEXTVIEWCOLOR));
 			answers.get(i).setText(answersString.get(i));
-			int textWidth = (int) paint.measureText(answersString.get(i)) + 60;
-			int textHeight = height / 10;
-			int x = r.nextInt(rWidth - textWidth);
-			int y = r.nextInt(rHeight - textHeight);
-			if (!isColiding(x, y, textWidth, textHeight)) {
-
-				boundsRect.add(new Rect(x, y, x + textWidth, y + textHeight));
-				answers.get(i).setX(x);
-				answers.get(i).setY(y);
-				answers.get(i).setBackground(getGradientDrawable());
-				++i;
-			}
-
 		}
 
-	}
-
-	private void setButton() {
-
-		int w = width / 3;
-		int h = height / 4;
-		playButton.setX(w);
-		playButton.setY(h);
-		Rect rect = new Rect(w, h, w + 200, h + 200);
-		boundsRect.add(rect);
-	}
-
-	private boolean isColiding(int x, int y, int width, int height) {
-
-		for (Rect bounds : boundsRect) {
-
-			if (bounds.left < x + width && bounds.left + bounds.width() > x
-					&& bounds.top < y + height
-					&& bounds.height() + bounds.top > y) {
-				return true;
-			}
-		}
-		return false;
-	}
-
-	private ArrayList<Integer> generateColors() {
-
-		ArrayList<Integer> colors = new ArrayList<Integer>();
-		colors.add(Color.rgb(235, 77, 77));
-		colors.add(Color.rgb(88, 243, 129));
-		colors.add(Color.rgb(192, 142, 213));
-		colors.add(Color.rgb(137, 170, 220));
-		colors.add(Color.rgb(88, 243, 129));
-		// colors.add(Color.rgb(244, 252, 244));
-		colors.add(Color.rgb(255, 255, 102));
-
-		return colors;
 	}
 
 	private void setAnswers() {
@@ -196,16 +146,13 @@ public class ChooseCharacterFromSoundActivity extends Activity implements Choose
 
 	}
 
-	private GradientDrawable getGradientDrawable() {
+	private GradientDrawable getGradientDrawable(int color) {
 
-		int i = colors.get(r.nextInt(colors.size()));
 		GradientDrawable gd = new GradientDrawable();
-		gd.setColor(i);
+		gd.setColor(color);
 		gd.setCornerRadius(10);
 		gd.setShape(GradientDrawable.OVAL);
-		// gd.setStroke(1, 0xFF000000);
 		gd.setStroke(2, Color.BLACK, 5, 5);
-
 		return gd;
 
 	}
@@ -253,8 +200,10 @@ public class ChooseCharacterFromSoundActivity extends Activity implements Choose
 							.getParent();
 					draggedImageViewParentLayout.removeView(draggedImageView);
 					LinearLayout bottomLinearLayout = (LinearLayout) receivingLayoutView;
-					LayoutParams lp=new LayoutParams(Gravity.CENTER);
-					bottomLinearLayout.setLayoutParams(findViewById(R.id.choose_character_from_sound_drop_pool).getLayoutParams());
+					LayoutParams lp = new LayoutParams(Gravity.CENTER);
+					bottomLinearLayout.setLayoutParams(findViewById(
+							R.id.choose_character_from_sound_drop_pool)
+							.getLayoutParams());
 					bottomLinearLayout.addView(draggedImageView, lp);
 					draggedImageView.setVisibility(View.VISIBLE);
 					mMediaPlayer.start();
@@ -284,24 +233,24 @@ public class ChooseCharacterFromSoundActivity extends Activity implements Choose
 	@Override
 	public void setAnswer(Item item) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void gameOver() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void wrongAnswer() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void setOfferedAnswers(Set<String> allOfferedLetters) {
 		// TODO Auto-generated method stub
-		
+
 	}
 }
