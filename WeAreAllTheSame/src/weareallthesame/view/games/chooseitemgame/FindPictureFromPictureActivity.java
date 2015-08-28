1ce8504157ab4d64c5f9311fc1b483a04328d4e4
Marija <Marija@Marija-PC>
2015-08-28 22:01:54 +0200

    find picture from picture dosreduvanje

--- WeAreAllTheSame/src/weareallthesame/view/games/chooseitemgame/FindPictureFromPictureActivity.java
+++ WeAreAllTheSame/src/weareallthesame/view/games/chooseitemgame/FindPictureFromPictureActivity.java
@@ -1,45 +1,65 @@
 package weareallthesame.view.games.chooseitemgame;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import weareallthesame.model.items.Item;
 import weareallthesame.view.R;
 import android.app.Activity;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.util.DisplayMetrics;
+import android.widget.GridView;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 public class FindPictureFromPictureActivity extends Activity implements ChooseItemViewInterface {
 
+	
+	private ImageView answer;
+	private ArrayList<ImageView> offeredAnswersImages;
+	private ArrayList<String> offeredImagesResources;
+	private DisplayMetrics displayMetrics;
+	private MediaPlayer mMediaPlayer;
+	private int width,height;
+	private GridView answersContainer;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_find_picture_from_picture);
+		
+		getMetrics();
+		getMediaPlayer();
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.find_picture_from_picture, menu);
-		return true;
+	private void getMetrics() {
+		displayMetrics = new DisplayMetrics();
+		getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+		width = displayMetrics.widthPixels;
+		height = displayMetrics.heightPixels;
 	}
 
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
+	private void getMediaPlayer() {
+
+		mMediaPlayer = new MediaPlayer();
+		mMediaPlayer = MediaPlayer.create(this, R.raw.slow_whoop_bubble_pop);
+		mMediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+
 	}
 
+	private void initializeViews() {
+		
+		answersContainer=(GridView) findViewById(R.id.find_the_picture_from_the_picture_answers_container);
+		answer=(ImageView) findViewById(R.id.find_picture_from_picture_picture);
+
+	}
 	@Override
 	public void setAnswer(Item answer) {
 		// TODO Auto-generated method stub
+		
+	this.answer.setBackgroundResource(getResources().getIdentifier(answer.getResourceNames().get("picture"), "raw", this.getPackageName()));
 		
 	}
 
