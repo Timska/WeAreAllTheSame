207fc0fc758df184a430d084f484f982b3135430
MarijaNikolova <marijanikolova93@hotmail.com>
2015-08-29 16:00:17 +0200

    find word from picture

--- WeAreAllTheSame/src/weareallthesame/view/games/chooseitemgame/FindTheWordFromThePictureActivity.java
+++ WeAreAllTheSame/src/weareallthesame/view/games/chooseitemgame/FindTheWordFromThePictureActivity.java
@@ -1,7 +1,7 @@
 package weareallthesame.view.games.chooseitemgame;
 
 import java.util.ArrayList;
-import java.util.List;
+import java.util.Iterator;
 import java.util.Set;
 
 import weareallthesame.model.ApplicationInterface;
@@ -10,43 +10,57 @@ import weareallthesame.view.R;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
+import android.util.DisplayMetrics;
+import android.widget.GridView;
 import android.widget.ImageView;
 
-public class FindTheWordFromThePictureActivity extends Activity implements ChooseItemViewInterface {
+public class FindTheWordFromThePictureActivity extends Activity implements
+		ChooseItemViewInterface {
 
 	private static final long serialVersionUID = -2816647589304154272L;
 
-	private Item answers;
-	
-	private Button answerOne;
-	private Button answerTwo;
-	private Button answerThree;
-	private Button answerFour;
-	
-	private ImageView picture;
+	private GridView answersContainer;
+	private DisplayMetrics displayMetrics;
 	private ApplicationInterface appInterface;
-	
+	private ArrayList<Integer> answerResources;
+	private int width, height;
+	private ArrayList<String> answers;
+	private ImageView imgPicture;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
-		openGame();
-		
 		setContentView(R.layout.activity_find_the_word_from_the_picture);
-	
-	
+
+		initializeViews();
+		openGame();
+
+	}
+
+	private void initializeViews() {
+
+		answersContainer = (GridView) findViewById(R.id.find_word_from_picture_answers_container);
+		imgPicture = (ImageView) findViewById(R.id.find_word_from_picture_picture);
 	}
 	
+	
+
 	private void openGame() {
 		Intent intent = getIntent();
 		String gameType = intent.getStringExtra("gameType");
 		ArrayList<String> gameTags = intent.getStringArrayListExtra("gameTags");
-		appInterface = (ApplicationInterface) intent.getSerializableExtra("appInterface");
-		try{
-			appInterface.openGame(gameType, gameTags.iterator(), this, this.getResources().getString(R.string.choose_character_from_sound_task_description));
-		}
-		catch(Exception e){
+		appInterface = (ApplicationInterface) intent
+				.getSerializableExtra("appInterface");
+		try {
+			appInterface
+					.openGame(
+							gameType,
+							gameTags.iterator(),
+							this,
+							this.getResources()
+									.getString(
+											R.string.choose_character_from_sound_task_description));
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
@@ -54,24 +68,32 @@ public class FindTheWordFromThePictureActivity extends Activity implements Choos
 	@Override
 	public void setAnswer(Item answer) {
 		// TODO Auto-generated method stub
-		
+		imgPicture.setBackgroundResource(getResources().getIdentifier(answer.getResourceNames().get("picture"), "raw", this.getPackageName()));
 	}
 
 	@Override
 	public void setOfferedAnswers(Set<Item> offeredAnswers) {
 		// TODO Auto-generated method stub
+
+		answers=new ArrayList<String>();
+		Iterator<Item> it=offeredAnswers.iterator();
+		while(it.hasNext()){
+			answers.add(it.next().getName());
+		}
+		a
+		
 		
 	}
 
 	@Override
 	public void gameOver() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void wrongAnswer() {
 		// TODO Auto-generated method stub
-		
+
 	}
 }
