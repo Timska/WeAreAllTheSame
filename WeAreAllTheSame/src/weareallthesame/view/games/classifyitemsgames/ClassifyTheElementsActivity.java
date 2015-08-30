99b7f4d1f3767dbef5cfc358dedb6a980ba87ead
MarijaNikolova <marijanikolova93@hotmail.com>
2015-08-30 15:06:15 +0200

    razdeli zborovi

--- WeAreAllTheSame/src/weareallthesame/view/games/classifyitemsgames/ClassifyTheElementsActivity.java
+++ WeAreAllTheSame/src/weareallthesame/view/games/classifyitemsgames/ClassifyTheElementsActivity.java
@@ -1,50 +1,71 @@
 package weareallthesame.view.games.classifyitemsgames;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.Map;
 import java.util.Set;
 
 import weareallthesame.model.ApplicationInterface;
 import weareallthesame.model.items.Item;
 import weareallthesame.view.R;
+import weareallthesame.view.games.chooseitemgame.AnswerShapeDrawableSurfaceView;
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Color;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.widget.GridView;
 import android.widget.LinearLayout;
-import android.widget.TextView;
 
 public class ClassifyTheElementsActivity extends Activity implements
 		ClassifyItemsViewInterface {
 
 	private final static int COLORGROUPONE = Color.rgb(247, 129, 129);
 	private final static int COLORGROUPTWO = Color.rgb(129, 185, 248);
-	private ArrayList<TextView> elements;
+	private ArrayList<String> elements;
 	private ArrayList<String> elementsGroupOne;
 	private ArrayList<String> elementsGroupTwo;
 	private LinearLayout groupOne;
 	private LinearLayout groupTwo;
 	private GridView container;
 	private ApplicationInterface appInterface;
-
+	private DisplayMetrics displayMetrics;
+	private int width, height;
+	private String longestAnswer;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_classify_the_elements);
 
-		openGame();
-
-		elements = new ArrayList<TextView>();
+		getMetrics();
+		initializeViews();
+		elements=new ArrayList<String>();
 		elementsGroupOne = new ArrayList<String>();
 		elementsGroupTwo = new ArrayList<String>();
+		openGame();
+	
 
-		container = (GridView) findViewById(R.id.classify_elements_container);
-		groupOne = (LinearLayout) findViewById(R.id.classify_elements_group_one);
-		groupTwo = (LinearLayout) findViewById(R.id.classify_elements_group_two);
+		
+
+		
 
 	}
 
+	private void getMetrics() {
+		displayMetrics = new DisplayMetrics();
+		getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+		width = displayMetrics.widthPixels;
+		height = displayMetrics.heightPixels;
+	}
+	private void initializeViews(){
+		container = (GridView) findViewById(R.id.classify_elements_container);
+		groupOne = (LinearLayout) findViewById(R.id.classify_elements_group_one);
+		groupTwo = (LinearLayout) findViewById(R.id.classify_elements_group_two);
+	}
+	
+	
 	private void openGame() {
 		Intent intent = getIntent();
 		String gameType = intent.getStringExtra("gameType");
@@ -67,6 +88,23 @@ public class ClassifyTheElementsActivity extends Activity implements
 	public void setOrUpdate(Set<Item> offeredItems) {
 		// TODO Auto-generated method stub
 
+		elements=new ArrayList<String>();
+		longestAnswer="";
+		Iterator<Item> it=offeredItems.iterator();
+		while(it.hasNext()){
+			String text=it.next().getName();
+			elements.add(text);
+			if (text.length() > longestAnswer.length()) {
+				longestAnswer = text;
+			}
+		}
+		Typeface tf = Typeface.createFromAsset(getAssets(),
+				"fonts/amerika_.ttf");
+		int txtWidth = this.width / 3;
+		int txtHeight = this.height / 10;
+		
+		container.setAdapter(new ClassifyItemsTextViewAdapter(this, elements,tf, txtWidth, txtHeight, COLORGROUPONE, longestAnswer));
+		
 	}
 
 	@Override
