026fe547042ed3a3e94bd145efa5bdeec0d6a379
MarijaNikolova <marijanikolova93@hotmail.com>
2015-08-31 19:19:23 +0200

    question game

--- WeAreAllTheSame/src/weareallthesame/view/games/questiongame/QuestionGameActivity.java
+++ WeAreAllTheSame/src/weareallthesame/view/games/questiongame/QuestionGameActivity.java
@@ -1,16 +1,21 @@
 package weareallthesame.view.games.questiongame;
 
 import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Iterator;
 import java.util.Random;
+import java.util.Set;
 
 import weareallthesame.model.ApplicationInterface;
+import weareallthesame.model.items.Item;
+import weareallthesame.view.GameOverChoiceActivity;
 import weareallthesame.view.R;
+import weareallthesame.view.games.chooseitemgame.ChooseItemTextViewAdapter;
 import android.app.Activity;
 import android.content.ClipData;
 import android.content.ClipDescription;
 import android.content.Intent;
 import android.graphics.Color;
+import android.graphics.Typeface;
 import android.graphics.drawable.GradientDrawable;
 import android.graphics.drawable.GradientDrawable.Orientation;
 import android.media.AudioManager;
@@ -24,125 +29,97 @@ import android.view.View.DragShadowBuilder;
 import android.view.View.OnDragListener;
 import android.view.View.OnLongClickListener;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.GridView;
 import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.AdapterView.OnItemClickListener;
 
-public class QuestionGameActivity extends Activity {
+public class QuestionGameActivity extends Activity implements
+		QuestionViewInterface {
 
+	private static final int COLORTEXTVIEWS = Color.rgb(255, 175, 0);
 	private ArrayList<TextView> txtAnswers;
 	private ArrayList<Integer> colors;
 	private TextView txtQuestion;
 	private TextView txtAnswer;
 	private String guestion;
+	private ArrayList<String> answers;
 	private Random r = new Random();
 	private DisplayMetrics displayMetrics;
-	private int width, height,w;
+	private int width, height, w;
 	private MediaPlayer mMediaPlayer;
 	private ApplicationInterface appInterface;
+	private GridView answersContainer;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_question_game);
 
+		getMetrics();
+		getMediaPlayer();
+		initializeViews();
 		openGame();
-		
+
+		txtAnswer = (TextView) findViewById(R.id.question_game_answer);
+
+	}
+
+	private void getMetrics() {
 		displayMetrics = new DisplayMetrics();
 		getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
 		width = displayMetrics.widthPixels;
 		height = displayMetrics.heightPixels;
-		w=width/4;
+		w = width / 4;
+	}
+
+	private void getMediaPlayer() {
 		mMediaPlayer = new MediaPlayer();
 		mMediaPlayer = MediaPlayer.create(this, R.raw.slow_whoop_bubble_pop);
 		mMediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
-		
-		txtAnswer=(TextView) findViewById(R.id.question_game_answer);
-		txtAnswers = new ArrayList<TextView>();
-		txtAnswers.add((TextView) findViewById(R.id.question_game_answer_one));
-		txtAnswers.add((TextView) findViewById(R.id.question_game_answer_two));
-		txtAnswers.add((TextView) findViewById(R.id.question_game_answer_three));
-		txtAnswers.add((TextView) findViewById(R.id.question_game_answer_four));
-
-		colors = generateColors();
-		txtQuestion = (TextView) findViewById(R.id.question_game_question);
-		txtQuestion.setBackground(getGradientDrawable());
-		txtQuestion.setText("Kako se vika bratot na Marija?");
-		txtQuestion.setGravity(Gravity.CENTER);
-		setAnswers();
-		
-		txtAnswer.setBackground(getGradientDrawable());
-		txtAnswer.setOnDragListener(new MyDragListener());
-		
-		for(int i=0;i<txtAnswers.size();++i){
-			txtAnswers.get(i).setOnLongClickListener(new MyClickListener());
-		}
 
 	}
-	
+
+	private void initializeViews() {
+		txtQuestion = (TextView) findViewById(R.id.question_game_question);
+		answersContainer = (GridView) findViewById(R.id.question_game_answers_container);
+		txtQuestion.setBackground(getGradientDrawable(COLORTEXTVIEWS));
+		txtQuestion.setText("Kako se vika bratot na Marija?");
+		txtQuestion.setGravity(Gravity.CENTER);
+
+	}
+
 	private void openGame() {
 		Intent intent = getIntent();
 		String gameType = intent.getStringExtra("gameType");
 		ArrayList<String> gameTags = intent.getStringArrayListExtra("gameTags");
-		appInterface = (ApplicationInterface) intent.getSerializableExtra("appInterface");
-		try{
-			appInterface.openGame(gameType, gameTags.iterator(), this, this.getResources().getString(R.string.choose_character_from_sound_task_description));
-		}
-		catch(Exception e){
+		appInterface = (ApplicationInterface) intent
+				.getSerializableExtra("appInterface");
+		try {
+			appInterface.openGame(
+					gameType,
+					gameTags.iterator(),
+					this,
+					this.getResources().getString(
+							R.string.question_game_task_description));
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
 
-	private void setAnswers() {
-		for (int i = 0; i < txtAnswers.size(); ++i) {
-			TextView tx = txtAnswers.get(i);
+	private GradientDrawable getGradientDrawable(int color) {
 
-			tx.setBackground(getGradientDrawable());
-			tx.setText("Martin");
-			tx.setTag("Incorrect");
-			tx.setWidth(w);
-			w+=(width/4);
-
-		}
-		txtAnswers.get(0).setTag("Correct");
-		Collections.shuffle(txtAnswers);
-	}
-
-	private ArrayList<Integer> generateColors() {
-
-		ArrayList<Integer> colors = new ArrayList<Integer>();
-		colors.add(Color.rgb(235, 77, 77));
-		colors.add(Color.rgb(88, 243, 129));
-		colors.add(Color.rgb(192, 142, 213));
-		colors.add(Color.rgb(137, 170, 220));
-		colors.add(Color.rgb(88, 243, 129));
-		// colors.add(Color.rgb(244, 252, 244));
-		colors.add(Color.rgb(255, 255, 102));
-
-		
-		return colors;
-	}
-
-	private GradientDrawable getGradientDrawable() {
-		
-		/*int colorsInt[]=new int[colors.size()];
-		Collections.shuffle(colors);
-		for(int i=0;i<colors.size()-2;++i){
-			colorsInt[i]=colors.get(i);
-		} */
-		int[] colorsInt={Color.rgb(235, 77, 77),Color.rgb(255, 255, 102)};
-
-		int i = colors.get(r.nextInt(colors.size()));
 		GradientDrawable gd = new GradientDrawable();
 		gd.setOrientation(Orientation.LEFT_RIGHT);
-		gd.setColors(colorsInt);
-		//gd.setColor(i);
+		gd.setColor(color);
 		gd.setCornerRadius(10);
 		gd.setShape(GradientDrawable.RECTANGLE);
-		// gd.setStroke(1, 0xFF000000);
 		gd.setStroke(2, Color.BLACK, 5, 5);
-
 		return gd;
 
 	}
+
 	private final class MyClickListener implements OnLongClickListener {
 
 		@Override
@@ -174,7 +151,7 @@ public class QuestionGameActivity extends Activity {
 			case DragEvent.ACTION_DRAG_STARTED:
 				break;
 			case DragEvent.ACTION_DRAG_ENTERED:
-				receivingLayoutView.setBackground(getGradientDrawable());
+				// receivingLayoutView.setBackground(getGradientDrawable());
 				break;
 			case DragEvent.ACTION_DRAG_LOCATION:
 				break;
@@ -209,5 +186,95 @@ public class QuestionGameActivity extends Activity {
 			return true;
 		}
 
+	}
+
+	@Override
+	public void setQuestion(String question) {
+		// TODO Auto-generated method stub
+		txtQuestion.setText(question);
+	}
+
+	@Override
+	public void setOfferedAnswers(Set<String> offeredAnswers) {
+		// TODO Auto-generated method stub
+		Iterator<String> it = offeredAnswers.iterator();
+		answers = new ArrayList<String>();
+		while (it.hasNext()) {
+			answers.add(it.next());
+		}
+		Typeface tf = Typeface.createFromAsset(getAssets(),
+				"fonts/amerika_.ttf");
+
+		answersContainer.setAdapter(new QuestionGameTextViewAdapter(this,
+				answers, tf, COLORTEXTVIEWS));
+
+		answersContainer.setOnItemClickListener(new OnItemClickListener() {
+
+			@Override
+			public void onItemClick(AdapterView<?> parent, View view,
+					int position, long id) {
+				// TODO Auto-generated method stub
+
+				try {
+					appInterface.executeCommand("ChooseString",
+							answers.get(position));
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+
+			}
+		});
+	}
+
+	@Override
+	public void gameOver() {
+		Intent intent = new Intent(this, GameOverChoiceActivity.class);
+		startActivityForResult(intent, 0);
+	}
+
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		if (requestCode == 0) {
+			if (resultCode == RESULT_OK) {
+				String result = data.getExtras().getString("result");
+				if (result.equals("NEW")) {
+
+					Intent intent = new Intent(this, this.getClass());
+					try {
+						intent.putExtra("gameType",
+								appInterface.getCurrentGameType());
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+					intent.putStringArrayListExtra("gameTags",
+							appInterface.getCurrentGameTags());
+					intent.putExtra("appInterface", appInterface);
+
+					try {
+						appInterface.exitGame();
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+
+					startActivity(intent);
+
+					finish();
+				} else if (result.equals("BACK")) {
+					try {
+						appInterface.exitGame();
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+					finish();
+				}
+			}
+		}
+	}
+
+	@Override
+	public void wrongAnswer() {
+
+		Toast.makeText(getApplicationContext(), "Неточен одговор",
+				Toast.LENGTH_SHORT).show();
 	}
 }
